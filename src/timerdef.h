c44710b861da8ef865f8094f8f94a9241bb53c94
Matthias Larisch <mail@matthias-larisch.de>
2012-09-30 21:15:28 +0200

    everything NOT written by other programmers is now published under GPLv2 unless marked otherwise. Careful, not everything here is done by me

---
+++ src/timerdef.h
@@ -0,0 +1,15 @@
+/*
+ * timerdef.h
+ *
+ *  Created on: 18.12.2011
+ *      Author: matthias
+ */
+
+#ifndef TIMERDEF_H_
+#define TIMERDEF_H_
+
+
+typedef void (*TimerCallback)(void);
+
+
+#endif /* TIMERDEF_H_ */
